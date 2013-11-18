33b0d1d2676edfa8ffc1fde8ae05c7bb8b1cf02b
pedro <pedro>
2013-11-18 18:55:56 +0000

    Alterações na view do pyramid_restler
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/LBBulk@768 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- lbbulk/view/restfulview.py
+++ lbbulk/view/restfulview.py
@@ -6,6 +6,6 @@ class RegCustomView(RESTfulView):
         id = self.request.matchdict['id']
         json_reg = self.requests.params['json_reg']
         json_reg = json.loads(json_reg)
-        ins = registro.insert().values(chave_externa=json_reg['id_reg', fullname='Jack Jones')
+        ins = registro.insert().values(chave_externa=json_reg['id_reg'])
         member = self.context.get_member(id)
         return self.render_to_response(member)
--- lbbulk/views.py
+++
@@ -1,7 +0,0 @@
-from pyramid.response import Response
-from pyramid.view import view_config
-
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'LBBulk'}
